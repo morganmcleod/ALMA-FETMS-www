36229f86b07d16b50203cf1fd56da1d7ad65ac54
Morgan McLeod <mmcleod@nrao.edu>
2013-02-13 16:50:30 -0500

    added fetms.osf.alma.cl to dbConnect.php

--- dbConnect/dbConnect.php
+++ dbConnect/dbConnect.php
@@ -1,8 +1,20 @@
 <?php
-$dbname     = 'alma_feic';
-$dbserver   = 'sql5.cv.nrao.edu';
-$dbusername = 'na_feic';
-$dbpassword = 'qSfUO65a';
+
+switch ($site_hostname){
+    case "fetms.osf.alma.cl":
+        $dbname     = 'fetms';
+        $dbserver   = 'fetms.osf.alma.cl';
+        $dbusername = 'fetms';
+        $dbpassword = '!fetms';
+        break;
+
+    default:
+        $dbname     = 'alma_feic';
+        $dbserver   = 'sql5.cv.nrao.edu';
+        $dbusername = 'na_feic';
+        $dbpassword = 'qSfUO65a';
+        break;
+}
 
 $db = mysql_pconnect($dbserver, $dbusername, $dbpassword)
 OR die ('Could not connect to MySQL: ' . mysql_connect_error() );
