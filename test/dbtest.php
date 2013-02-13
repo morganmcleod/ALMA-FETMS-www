83362beab6a5270b335daab68b99e1e091e73110
Morgan McLeod <mmcleod@nrao.edu>
2013-02-13 17:17:30 -0500

    test/dbtest.php

--- test/dbtest.php
+++ test/dbtest.php
@@ -5,6 +5,11 @@ require_once($site_dbConnect);
 require_once($site_config_main);
 
 echo "DB Test<br><br>";
+echo "site_root=$site_root<br>";
+echo "site_hostname=$site_hostname<br>";
+echo "dbserver=$dbserver<br>";
+echo "dbname=$dbname<br><br>";
+
 $q = "SELECT keyId, Description FROM Locations ORDER BY keyId ASC;";
 echo "Query: $q<br><br>";
 $r = @mysql_query($q,$db);
