2ed0ff7f247599cc4acc0d24a90d016aa22662f0
Morgan McLeod <mmcleod@nrao.edu>
2013-02-24 09:25:58 -0500

    better debugging in bp/main.c

--- FEConfig/bp/beameff/main.c
+++ FEConfig/bp/beameff/main.c
@@ -23,7 +23,7 @@
 #include "plotcircles.h"
 #include "nsi.h"
 
-int DEBUGGING = 0;
+int DEBUGGING = 1;
 
 int main(int argc, char *argv[])
 {
@@ -39,11 +39,11 @@ int main(int argc, char *argv[])
   int scanset_array[200];
   char printmsg[200];
 
-  sprintf(printmsg,"********************************************<br>",VersionNumber);
+  sprintf(printmsg,"********************************************<br>\n",VersionNumber);
   PRINT_STDOUT(printmsg);
-  sprintf(printmsg,"Beam Efficiency Calculator Version  %s<br>",VersionNumber);
+  sprintf(printmsg,"Beam Efficiency Calculator Version  %s<br>\n",VersionNumber);
   PRINT_STDOUT(printmsg);
-  sprintf(printmsg,"********************************************<br><br>",VersionNumber);
+  sprintf(printmsg,"********************************************<br>\n<br>\n",VersionNumber);
   PRINT_STDOUT(printmsg);
     
 
@@ -55,10 +55,10 @@ int main(int argc, char *argv[])
   //Create dictionary from the input text file
   scan_file_dict = iniparser_load(inputfile);
   num_scansets=GetScanSetNumberArray(scan_file_dict,&scanset_array,200);
-  sprintf(printmsg,"Input File: %s<br>",inputfile);
+  sprintf(printmsg,"Input File: %s<br>\n",inputfile);
 
   PRINT_STDOUT(printmsg);
-  sprintf(printmsg,"Number of scans: %d<br>",GetNumberOfScans(scan_file_dict));
+  sprintf(printmsg,"Number of scans: %d<br>\n",GetNumberOfScans(scan_file_dict));
   PRINT_STDOUT(printmsg);
   
   
@@ -66,25 +66,25 @@ int main(int argc, char *argv[])
   GetNSIValues(scan_file_dict);
   GetZ(scan_file_dict);
   GetOutputFilename(scan_file_dict,outfilename);
-  sprintf(printmsg,"Number of scan sets= %d<br>",num_scansets);
+  sprintf(printmsg,"Number of scan sets= %d<br>\n",num_scansets);
   PRINT_STDOUT(printmsg);
 
   for (i=0;i<num_scansets;i++){
     //Call efficiency module for each scan set. A scan set
     //consists of four scans at a certain band, frequency and
     //tilt angle.
-    sprintf(printmsg,"Getting efficiencies and plots, scanset %d of %d...<br>",i+1,num_scansets);
+    sprintf(printmsg,"Getting efficiencies and plots, scanset %d of %d...<br>\n",i+1,num_scansets);
     PRINT_STDOUT(printmsg);
     GetEfficiencies(scan_file_dict,scanset_array[i],outfilename);       
   }
-  PRINT_STDOUT("Generating pointing angle plots...<br>");
+  PRINT_STDOUT("Generating pointing angle plots...<br>\n");
   PlotCircles(scan_file_dict);
   RemoveKeys(scan_file_dict);
-  sprintf(printmsg,"Saving output to file: %s<br>",outfilename);
+  sprintf(printmsg,"Saving output to file: %s<br>\n",outfilename);
   PRINT_STDOUT(printmsg);
   SaveOutputFile(scan_file_dict, outfilename);
   iniparser_freedict(scan_file_dict);
-  PRINT_STDOUT("finished.<br>");
+  PRINT_STDOUT("finished.<br>\n");
   return 0;
 }
 
