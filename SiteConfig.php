a48feee07d676368e8efea50bd12f25c28d5c70e
Morgan McLeod <mmcleod@nrao.edu>
2013-02-28 19:21:35 -0300

    ini_set('display_errors', '1');

--- SiteConfig.php
+++ SiteConfig.php
@@ -1,6 +1,7 @@
 <?php
 
 date_default_timezone_set ('America/New_York');
+ini_set('display_errors', '1');
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 set_time_limit(1800);     // ridiculously long = 30 minutes!
 
