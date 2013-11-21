be588bda4ae9a4eb79cbb031fdb8019bd7c77ab4
Morgan McLeod <mmcleod@nrao.edu>
2013-11-21 16:21:43 -0500

    fix PAS reference data putting IF spectrum files in old data files location.

--- DatabaseDeliveryTest/PASFunction.php
+++ DatabaseDeliveryTest/PASFunction.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once(dirname(__FILE__) . '/../SiteConfig.php');
+
 function getPASfileName($ifchannel, $band, $lofreq) {
     if($ifchannel == '0') {
         $pol=0; $sb=1;
@@ -24,22 +26,23 @@ function generatePASfile($ifspec_keyId,$ifchannel,$band,$lofreq,$facility)
             fkSubHeader='$ifspec_keyId' AND fkFacility='$facility' ORDER BY Freq_Hz")
     or die("Could not get IfSpectrum data" .mysql_error());
 
+    include(site_get_config_main());
 
-	$filename="PASData/" . getPASfileName($ifchannel, $band, $lofreq);
-	$delimiter="\t";
+    $filename="$main_write_directory/PASData/" . getPASfileName($ifchannel, $band, $lofreq);
+    $delimiter="\t";
 
-	if(mysql_num_rows($getIfSpecData_query) > 0) {
-	  	$file=fopen($filename,'w') or die("Cant create file");
+    if(mysql_num_rows($getIfSpecData_query) > 0) {
+        $file=fopen($filename,'w') or die("Cant create file");
 
-	  	while($IfSpecData=mysql_fetch_array($getIfSpecData_query))
-	  	{
-	  		$data = $IfSpecData['Freq_Hz'];
-	  		$data .= $delimiter . $IfSpecData['Power_dBm'];
-	  		$data .= "\r\n";
-	  		fwrite($file,$data);
-	  	}
+        while($IfSpecData=mysql_fetch_array($getIfSpecData_query))
+        {
+            $data = $IfSpecData['Freq_Hz'];
+            $data .= $delimiter . $IfSpecData['Power_dBm'];
+            $data .= "\r\n";
+            fwrite($file,$data);
+        }
 
-	  	fclose($file);
-	}
-	return;
+        fclose($file);
+    }
+    return;
 }
