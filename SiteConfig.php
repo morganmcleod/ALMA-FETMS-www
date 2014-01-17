b267240a49b7c8c8e516928c7dd9a8774cfea137
Morgan McLeod <mmcleod@nrao.edu>
2014-01-17 17:35:12 -0300

    turned off E_NOTICE for OSF

--- SiteConfig.php
+++ SiteConfig.php
@@ -2,7 +2,7 @@
 
 date_default_timezone_set ('America/New_York');
 ini_set('display_errors', '1');
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
 set_time_limit(1800);     // ridiculously long = 30 minutes!
 
 // site_root defaults to the server's document root, but that won't work on any NRAO servers:
