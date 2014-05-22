df6a1e6822d139432ad266a7dfb9839832053bcc
Morgan McLeod <mmcleod@nrao.edu>
2014-05-22 16:02:41 -0400

    suppress more spin drawing plots for tabular data.

--- FEConfig/testdata/testdata.php
+++ FEConfig/testdata/testdata.php
@@ -216,7 +216,16 @@ if (($td->GetValue('PlotURL') == '')) {
         case 1:
         case 2:
         case 3:
-            // don't draw plots for CCA health check monitor data
+        case 4:
+        case 5:
+        case 8:
+        case 9:
+        case 10:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+            // don't draw plots for health check tabular data
             break;
 
         case 57:
