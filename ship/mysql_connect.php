9f8fdf90f6f3c1dc8897aabb80ef3765a22f2175
Morgan McLeod <mmcleod@nrao.edu>
2016-04-19 15:56:29 -0400

    Get /ship/mysql_connect.php creds from /conf/

--- ship/mysql_connect.php
+++ ship/mysql_connect.php
@@ -1,12 +1,17 @@
 <?php
+/*
+ * 2016-04-19 mtm Removed secret credentials from this file.  Get them from /conf/ instead.
+ * 2016-03-28 jee just fixed mysql_error - this was mysql_connect_error
+ *                also added pw here
+ */
+ 
+
+include("/home/safe.nrao.edu/conf/mtm-dbConnect.conf");
 
 $dbname     = 'shipping';
-$dbserver   = 'sql5.cv.nrao.edu';
-$dbusername = 'na_feic';
-$dbpassword = 'qSfUO65a';
 
 $dbc = mysql_connect($dbserver, $dbusername, $dbpassword)
-or die ('Could not connect to MySQL: ' . mysql_connect_error() );
+or die ('Could not connect to MySQL: ' . mysql_error() );
 mysql_select_db($dbname, $dbc);
 
 ?>
