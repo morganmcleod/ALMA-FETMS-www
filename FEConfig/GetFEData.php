427d24e332907d6c3fae875d2926c1895a20d951
Morgan McLeod <mmcleod@nrao.edu>
2017-01-20 15:39:56 -0500

    Rewrote queries for main page.

--- FEConfig/GetFEData.php
+++ FEConfig/GetFEData.php
@@ -4,6 +4,7 @@
 require_once(dirname(__FILE__) . '/../SiteConfig.php');
 require_once($site_classes . '/class.frontend.php');
 require_once($site_classes . '/class.fecomponent.php');
+require_once($site_libraries . '/array_column/src/array_column.php');
 require_once($site_dbConnect);
 require('dbGetQueries.php');
 
@@ -14,140 +15,146 @@ $getqueries=new dbGetQueries;
 if($ctype==100)
 {
     //Front Ends
-    $outstring = "[";
-    $rowcount = 0;
-    $Notes = "";
-    $TS = "";
+    $q = "SELECT keyFrontEnds, SN, Docs, MAX(FE_Config.keyFEConfig) AS keyConfig, FE_Config.TS
+          FROM Front_Ends, FE_Config
+          WHERE Front_Ends.keyFrontEnds = FE_Config.fkFront_Ends
+          GROUP BY SN
+          ORDER BY SN;";
 
-    $qfe = "SELECT keyFrontEnds, keyFacility FROM Front_Ends ORDER BY SN ASC;";
-    $rfe = @mysql_query($qfe,$db);
+    $rfe = mysql_query($q, $db);
 
-    while ($row = @mysql_fetch_array($rfe)){
+    $feRecs = array();
+    $configIds = FALSE;
 
-        $fe = new FrontEnd();
-        $fe->Initialize_FrontEnd($row[0], $row[1], FrontEnd::INIT_SLN);
+    while ($row = mysql_fetch_array($rfe)) {
+        $feRecs[] = $row;
+        if ($configIds)
+            $configIds .= ",";
+        else
+            $configIds = "";
+        $configIds .= $row['keyConfig'];
+    };
 
-        if ($rowcount == 0 ){
-            $outstring .= "{'SN':'".$fe->GetValue('SN')."',";
-        }
-        if ($rowcount > 0 ){
-            $outstring .= ",{'SN':'".$fe->GetValue('SN')."',";
-        }
-        $outstring .= "'config':'".$fe->feconfig->keyId."',";    ;
+    // Status, location, notes
+    $q = "SELECT A.fkFEConfig, A.TS, A.Notes
+          FROM FE_StatusLocationAndNotes A
+          LEFT OUTER JOIN FE_StatusLocationAndNotes B
+          ON (A.fkFEConfig = B.fkFEConfig AND A.keyId < B.keyId)
+          WHERE B.keyId IS NULL
+          AND A.fkFEConfig IN ($configIds);";
 
-        if (isset($fe->fesln)) {
-            if ($fe->fesln -> keyId > 0) {
-                $outstring .= "'Location':'".$fe->fesln->location."',";
-                $outstring .= "'Status':'".$fe->fesln->status."',";
-                $outstring .= "'Updated_By':'".$fe->fesln->GetValue('Updated_By')."',";
-                $TS = $fe->fesln->GetValue('TS');
-                $Notes = @mysql_real_escape_string(stripslashes($fe->fesln->GetValue('Notes')));
-            }
-            if ($fe->fesln -> keyId < 1){
-                $outstring .= "'Location':'',";
-                $outstring .= "'Status':'',";
-                $outstring .= "'Updated_By':'',";
-                $TS = $fe->GetValue('TS');
-                $Notes = '';
-            }
-        }
-        $outstring .= "'Docs':'" . FixHyperLink($fe->GetValue('Docs')). "',";
-        $outstring .= "'TS':'" . $TS . "',";
-        $outstring .= "'keyFacility':'" . $fe->GetValue('keyFacility') . "',";
-        $outstring .= "'Notes':'$Notes'}";
+    $rsl = mysql_query($q, $db);
 
-        unset($fe);
-        $rowcount += 1;
+    $slnRecs = array();
+
+    while ($row = mysql_fetch_array($rsl))
+        $slnRecs[] = $row;
+
+    $retJSON = FALSE;
+
+    // Build the JSON string from the front end records, finding the corresponding SLN records:
+    foreach ($feRecs as $row) {
+        if (!$retJSON)
+            $retJSON = "[";
+        else
+            $retJSON .= ",";
+
+        $slKey = array_search($row['keyConfig'], array_column($slnRecs, 'fkFEConfig'));
+
+        $retJSON .= "{'config':'" . $row['keyConfig'] . "'";
+        $retJSON .= ",'keyFacility':'40'";
+        $retJSON .= ",'SN':'" . $row['SN'] . "'";
+        $retJSON .= ",'TS':'" . (($slKey) ? $slnRecs[$slKey]['TS'] : $row['TS']) . "'";
+        $retJSON .= ",'Docs':'" . $row['Docs'] . "'";
+        $notes = (($slKey) ? $slnRecs[$slKey]['Notes'] : "");
+        $retJSON .= ",'Notes':'" . mysql_real_escape_string($notes) . "'}";
     }
-    $outstring .= "]";
-    echo $outstring;
-
+    $retJSON .= "]";
+    echo $retJSON;
 }
 else
 {
     //Components
+    $q = "SELECT A.keyId, A.Band, A.SN, A.TS
+          FROM FE_Components A
+          LEFT OUTER JOIN FE_Components B
+          ON (A.fkFE_ComponentType = B.fkFE_ComponentType
+            AND A.Band = B.Band
+            AND A.SN = B.SN
+            AND A.keyId < B.keyId)
+          WHERE A.fkFE_ComponentType = $ctype
+          AND B.keyId IS NULL
+          ORDER BY A.Band ASC, (0 + A.SN) ASC;";
 
-    /*
-    Band
-    SN
-    TS
-    Updated By
-    Status
-    Location
-    Notes
-    Front End
-     */
-    //$l = new Logger('GetFEData.txt');
-    //$l->WriteLogFile("test");
+    $rcm = mysql_query($q, $db);
 
-    $outstring = "[";
-    $rowcount = 0;
-    $Notes = "";
+    $cmRecs = array();
+    $configIds = FALSE;
 
-    $qfe = "SELECT keyId, keyFacility FROM FE_Components
-            WHERE fkFE_ComponentType = $ctype
-            ORDER BY Band ASC, (SN+0) ASC, keyId DESC;";
-    $rfe = @mysql_query($qfe,$db);
+    while ($row = mysql_fetch_array($rcm)) {
+        $cmRecs[] = $row;
+        if ($configIds)
+            $configIds .= ",";
+        else
+            $configIds = "";
+        $configIds .= $row['keyId'];
+    };
 
-    //$l->WriteLogFile($qfe);
-    $tempBand = FALSE;
-    $tempSN = FALSE;
+    // Status, location, notes
+    $q = "SELECT A.fkFEComponents, A.TS, A.Notes
+          FROM FE_StatusLocationAndNotes A
+          LEFT OUTER JOIN FE_StatusLocationAndNotes B
+          ON (A.fkFEComponents = B.fkFEComponents AND A.keyId < B.keyId)
+          WHERE B.keyId IS NULL
+          AND A.fkFEComponents IN ($configIds);";
 
-    while ($row = @mysql_fetch_array($rfe)){
+    $rsl = mysql_query($q, $db);
 
-        $c = new FEComponent();
-        $c->Initialize_FEComponent($row[0],$row[1]);
+    $slnRecs = array();
 
-        $Duplicate = 0;
-        if ($c->GetValue('Band') == $tempBand){
-            if ($c->GetValue('SN') == $tempSN){
-                $Duplicate = 1;
-            }
-        }
+    while ($row = mysql_fetch_array($rsl))
+        $slnRecs[] = $row;
 
-        if ($Duplicate == 0){
-            if ($rowcount == 0 ){
-                $outstring .= "{'SN':'".$c->GetValue('SN')."',";
-            }
-            if ($rowcount > 0 ){
-                $outstring .= ",{'SN':'".$c->GetValue('SN')."',";
-            }
-            $outstring .= "'config':'".$c->keyId."',";
+    // Find the max ConfigLink records which refer to the components in our list,
+    //  Also link in the Front_Ends record via FE_Config:
+    $q = "SELECT A.fkFE_Components, A.fkFE_Config, Front_Ends.SN
+          FROM FE_Config, Front_Ends, FE_ConfigLink A
+          LEFT OUTER JOIN FE_ConfigLink B
+          ON (A.fkFE_Components = B.fkFE_Components AND A.fkFE_Config < B.fkFE_Config)
+          WHERE B.fkFE_Config IS NULL
+          AND A.fkFE_Config = FE_Config.keyFEConfig
+          AND FE_Config.fkFront_Ends = Front_Ends.keyFrontEnds
+          AND A.fkFE_Components IN ($configIds)
+          ORDER BY fkFE_Components ASC;";
 
-            if (isset($c->sln)) {
-                if ($c->sln->keyId > 0){
-                    $outstring .= "'Location':'".$c->sln->location."',";
-                    $outstring .= "'Status':'".$c->sln->status."',";
-                    $outstring .= "'Updated_By':'".$c->sln->GetValue('Updated_By')."',";
-                    $Notes = @mysql_real_escape_string($c->sln->GetValue('Notes'));
-                    //$l->WriteLogFile("SLN: " . $c->sln->location . "," . $c->sln->status . "," . $c->sln->GetValue('Notes'));
-                }
+    $rcl = mysql_query($q, $db);
+    $clRecs = array();
 
-                if ($c->sln->keyId < 1){
-                    $outstring .= "'Location':'',";
-                    $outstring .= "'Status':'',";
-                    $outstring .= "'Updated_By':'',";
-                    $Notes = '';
-                }
-            }
-            $outstring .= "'Docs':'" . FixHyperLink($c->GetValue('Link1')). "',";
-            $outstring .= "'TS':'".$c->GetValue('TS')."',";
-            $outstring .= "'Band':'".$c->GetValue('Band')."',";
-            $outstring .= "'keyFacility':'".$c->GetValue('keyFacility')."',";
-            $outstring .= "'FESN':'".$c->FESN."',";
-            $outstring .= "'Notes':'$Notes'}";
+    while ($row = mysql_fetch_array($rcl))
+        $clRecs[] = $row;
 
-            $tempBand = $c->GetValue('Band');
-            $tempSN   = $c->GetValue('SN');
+    $retJSON = FALSE;
 
-            unset($c);
-            $rowcount += 1;
-        }
+    // Build the JSON string from the components records, finding the corresponding SLN and FE records:
+    foreach ($cmRecs as $row) {
+        if (!$retJSON)
+            $retJSON = "[";
+        else
+            $retJSON .= ",";
+
+        $slKey = array_search($row['keyId'], array_column($slnRecs, 'fkFEComponents'));
+        $clKey = array_search($row['keyId'], array_column($clRecs, 'fkFE_Components'));
+
+        $retJSON .= "{'config':'" . $row['keyId'] . "'";
+        $retJSON .= ",'keyFacility':'40'";
+        $retJSON .= ",'Band':'" . $row['Band'] . "'";
+        $retJSON .= ",'SN':'" . $row['SN'] . "'";
+        $retJSON .= ",'TS':'" . (($slKey) ? $slnRecs[$slKey]['TS'] : $row['TS']) . "'";
+        $retJSON .= ",'FESN':'" . (($clKey) ? $clRecs[$clKey]['SN'] : "") . "'";
+        $notes = (($slKey) ? $slnRecs[$slKey]['Notes'] : "");
+        $retJSON .= ",'Notes':'" . mysql_real_escape_string($notes) . "'}";
     }
-    $outstring .= "]";
-
-    //$l->WriteLogFile($outstring);
-
-    echo $outstring;
+    $retJSON .= "]";
+    echo $retJSON;
 }
 ?>
